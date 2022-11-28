3fccbce22ddd36293a6dfb5918763439e0e3bd81
Vogt935 <f-vogt@outlook.de>
2022-11-28 16:33:12 +0100

    Vers.0.0.1-Test

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,59 @@
 let mySIDButton = document.getElementById("my-steamid-button");
 let myHeading = document.querySelector("reqreturn");
 let mySID = document.getElementById("my-steam-ID");
-let fetchButton = document.getElementById("my-fetch-button");
+//let fetchButton = document.getElementById("my-fetch-button");
 
-let friendlist;
-
-
-
-
-
-fetchButton.onclick = () => {
-fetch("friends.json", {mode: "no-cors"})
-.then(response => {response.json();})
-.then(json => console.log(json));
+//Über-TODO: Import JSON; Get Game Infos; Show it all
+//TODO: Akzeptanz und Verarbeitung von JSONs im Steam-Style, wie bekomme ich das hin?
+//TODO: HTML/CSS: Darstellung Games-List, Darstellung Friendlist
+//TODO: Funktionsbuttons umsetzen (Darstellung Liste, sortieren usw)
 
 
-console.log("finished");
+//Friendslist:
+{
+// TODO: Zeitlich gesetzte neue Abfrage welche Freunde online sind?
+let fl ='{ "friends": [' +
+	'{ "steamid":"76561197966953159" , "relationship":"friend" , "friend_since":1394392802 },' +
+	'{ "steamid":"76561197969067270" , "relationship":"friend" , "friend_since":1452304416 } ,' +
+		'{ "steamid":"76561197969067270" , "relationship":"friend" , "friend_since":1452304416 } ,' +
+	'{ "steamid":"76561197979247125" , "relationship":"friend" , "friend_since":1620943081 } ]}';
+const objFL = JSON.parse(fl);
+
+let FLl=objFL.friends.length;
+//console.log(FLl);
+const lengthFl= FLl-1;
+if(lengthFl > 0) {
+	document.getElementById("friendlist").innerHTML="Es sind Freunde online:";
+	let i = 0;
+	while (i <= lengthFl) {
+		let id = "Test"+i;
+		//console.log(id);
+		document.getElementById(id).innerHTML= "Freund Nr. "+(i+1)+": SteamID: "+objFL.friends[i].steamid+", Friend since: "+Date(objFL.friends[i].friend_since);
+		i++;
+	}
 }
+}
+
+const steamnews =
+const objSN = JSON.parse(steamnews);
+
+console.log(objSN.appnews.appid);
+
+
+//fetchButton.onclick = () => {}
 
 
 //Steam ID of the websites user
 
-// Change saved User Steam ID 
+
+// Change saved User Steam ID
+{
 mySIDButton.onclick = () => {
   setUserName();
-
+  alert("User-ID geändert!")
 var response;
-
-
-
 };
-
+}
 
 //function to set the Users Steam ID and save it as "name" in local storage
 function setUserName() {
@@ -44,6 +67,7 @@ function setUserName() {
 }
 
 //request to save a Steam ID if there is non in local storage
+{
 if (!localStorage.getItem("name")) {
   mySID.textContent = "Nicht angemeldet!";
   myHeading.textContent = "";
@@ -52,7 +76,7 @@ if (!localStorage.getItem("name")) {
   myHeading.textContent = `Angemeldet mit Steam-ID: ${storedName}`;
   mySID.textContent = "Willkommen zurück, Fabs3953";
 }
-
+}
 
 
 
@@ -84,12 +108,3 @@ function getGameStats(appid) {
 	serRequestGameInfo = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=440&key=5FEC780FE57D3BBEA4B74CDA91C7C72E&steamid="+appid;
 	console.log(serRequestGameInfo);
 }
-
-
-
-
-
-
-
-
-
