b0a7d3c73116d290a361eb92c2288273cf934170
Vogt935 <f-vogt@outlook.de>
2023-02-05 14:58:56 +0100

    Update tableJS.js

--- scripts/tableJS.js
+++ scripts/tableJS.js
@@ -7,10 +7,10 @@ function buildGamesList() {
     
     filteredGamesList.forEach((element) => {delete element.img_icon_url; delete element.has_community_visible_stats; delete element.playtime_linux_forever; delete element.playtime_mac_forever; delete element.playtime_windows_forever; delete element.rtime_last_played; delete element.has_leaderboards; delete element.content_descriptorids; delete element.playtime_2weeks});
     
-    filteredGamesList.forEach(function(conversionToHours){
+    filteredGamesList.forEach((element) => function(conversionToHours){
         filteredGamesList.playtime_forever = filteredGamesList.playtime_forever / 60;
     });  
-    filteredGamesList.forEach(function(addActingUser) {
+    filteredGamesList.forEach((element) => function(addActingUser) {
         filteredGamesList.owners = actingUser;
     });
     
