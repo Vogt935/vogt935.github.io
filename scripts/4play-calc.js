e096e23c5963719ed3be04bf398fa4c239ec688d
Vogt935 <f-vogt@outlook.de>
2023-10-15 21:06:27 +0200

    updated 4P to same engine as 5P

--- scripts/4play-calc.js
+++ scripts/4play-calc.js
@@ -1,54 +1,63 @@
-var player1 ={name: "Spieler 1", value: 0, id: "player1"};
-var player2 ={name: "Spieler 2", value: 0, id: "player2"};
-var player3 ={name: "Spieler 3", value: 0, id: "player3"};
-var player4 ={name: "Spieler 4", value: 0, id: "player4"};
+let drin1= "";
+let drin2= "";
+let drin3= "";
+
+let raus1= "";
+counter = 0;
+turnoverCounter=0;
 
 
+
+
+
+function reshuffle(){
+    
+    if (drin1 == ""){
+        alert("Bitte Namen eingeben und bestätigen!");
+    }
+    
+    else{
+        
+        document.getElementById("box-Mitspieler").style.display="block";
+        document.getElementById("box-Counter").style.display="block";
+        
+        let wechselkandidat1;
+        let wechselkandidat2;
+
+        
+        wechselkandidat1 = raus1;
+        raus1 = drin3;
+        drin3 = drin2;
+        drin2 = drin1;
+        drin1 = wechselkandidat1;
+
+        
+        counter= counter+1;
+        if (counter % 4 ==0 ){
+            turnoverCounter++;
+        }
+        document.getElementById("counter").textContent=counter;
+        document.getElementById("turnoverCounter").textContent=turnoverCounter;
+
+        document.getElementById("drin1").textContent=drin1;
+        document.getElementById("drin2").textContent=drin2;
+        document.getElementById("drin3").textContent=drin3;
+        document.getElementById("raus1").textContent=raus1;
+
+    }
+}
 
 function teamInput(){
     
-    player1.name= document.getElementById("n1").value;
-    player2.name= document.getElementById("n2").value;
-    player3.name= document.getElementById("n3").value;
-    player4.name= document.getElementById("n4").value;
-    document.getElementById("player1").innerHTML = player1.name+"   "+player1.value;
-    document.getElementById("player2").innerHTML = player2.name+"   "+player2.value;
-    document.getElementById("player3").innerHTML = player3.name+"   "+player3.value;
-    document.getElementById("player4").innerHTML = player4.name+"   "+player4.value;
-    document.getElementById("4pBox").style.display="block"; 
+    document.getElementById("box-Mitspieler").style.display="none";
+    document.getElementById("box-Counter").style.display="none";
+    drin1= document.getElementById("n1").value;
+    drin2= document.getElementById("n2").value;
+    drin3= document.getElementById("n3").value;
+    raus1= document.getElementById("n4").value;
+
+
+    counter = 0;
+    turnoverCounter = 0;
+    alert("Namen bestätigt!");
 }
-
-
-function addValue(player){
-    player.value++;
-    
-    document.getElementById(player.id).innerHTML = player.name+"   "+player.value;
-}
-
-function nullen(){
-    player1.value=0;
-    player2.value=0;
-    player3.value=0;
-    player4.value=0;
-    document.getElementById("player1").innerHTML = player1.name+"   "+player1.value;
-    document.getElementById("player2").innerHTML = player2.name+"   "+player2.value;
-    document.getElementById("player3").innerHTML = player3.name+"   "+player3.value;
-    document.getElementById("player4").innerHTML = player4.name+"   "+player4.value;
-
-}
-
-
-function reset(){
-    player1.value=0;
-    player2.value=0;
-    player3.value=0;
-    player4.value=0;
-    ausblenden();
-}
-
-
-function ausblenden(){
-    document.getElementById("4pBox").style.display="none";
-    console.log("ausgeblendet");
-}
-//ausblenden();
